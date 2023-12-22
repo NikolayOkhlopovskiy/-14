4daf564f2aa823ddc2630efd43b272ff3997a142
NikolayOkhlopovskiy <154047449+NikolayOkhlopovskiy@users.noreply.github.com>
2023-12-22 11:00:00 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,30 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+                        //В зависимости от выбранной буквы русской буквы Т или П или К или О вывести название фигуры, вычислить Периметр и Площадь и Вывести их.
+            //Console.Write("введите букву  - ");
+            //char letter = char.Parse(Console.ReadLine());
+            //switch(letter)
+            //{
+            //   case 'Т': case 'т':
+            //        Console.Write("Это треугольник");
+            //        break;
+               
+            //    case 'П': case 'п':
+            //        Console.Write("Это Прямоугольник");
+            //        break;
+                
+            //    case 'К': case 'к':
+            //        Console.Write("Это квадрат");
+            //        break;
+            //    //case 'О': case 'о':
+            //        Console.Write("Это окружность");
+            //        break;
+            //    default:
+            //        Console.Write("Такой фигуры нет");
+            //        break;
+            //}
+            //Console.Read();
         }
     }
 }
